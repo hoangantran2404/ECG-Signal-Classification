0e8e978d4b6ba491679eb6ea92ef1e7b7d29a8af
Ngo Tran Hoang An <ngotranhoangan2007@gmail.com>
2026-02-28 11:54:27 -0600

    Refactor ALU module inputs and outputs

--- RTL/ALU.v
+++ RTL/ALU.v
@@ -18,34 +18,26 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module ALU #(
-    parameter DATA_WIDTH=16,
-    parameter ADDR_WIDTH=8
+    parameter DATA_WIDTH=16
 )
 (
     input wire                          clk,
     input wire                          rst_n,
-    input wire                          start_in,
-    input wire signed [DATA_WIDTH-1:0]  weight_in,
-    input wire signed [DATA_WIDTH-1:0]  activation_in,
+    input wire signed [DATA_WIDTH-1:0]  Filter_in,
+    input wire signed [DATA_WIDTH-1:0]  IA_in,
+    input wire [2:0]                    CTRL_counter_in,
 
-    input wire signed [DATA_WIDTH-1:0]  bias_in,
-    input wire                          bias_valid_in,
-    input wire                          ReLU_en_in,
-    input wire                          CTRL_send_enable_in,   
-    input wire                          CTRL_gap_enable_in,
 
 
     output wire signed [DATA_WIDTH-1:0] ALU_data_out,
     output wire                         ALU_data_valid_out
 );
-    wire signed [DATA_WIDTH-1:0] MAC_out_wr;
-    wire                         MAC_valid_wr;
+    wire signed [DATA_WIDTH-1:0]        MAC_out_wr;
+    wire                                MAC_valid_wr;
     
-    assign ALU_data_out         = (CTRL_send_enable_in && MAC_valid_wr)? MAC_out_wr : 0;
-    assign ALU_data_valid_out   = (CTRL_send_enable_in && MAC_valid_wr) ? 1 : 0;
+    assign ALU_data_out         = (MAC_valid_wr)? MAC_out_wr : 0;
+    assign ALU_data_valid_out   = (CTRL_counter_in == 4 && MAC_valid_wr) ? 1 : 0;
 
     MAC #(
         .DATA_WIDTH(DATA_WIDTH),
@@ -53,12 +45,10 @@ module ALU #(
     ) MAC_inst (
         .clk                    (clk            ),
         .rst_n                  (rst_n          ),
-        .CTRL_gap_enable_in     (CTRL_gap_enable_in),
-        .ReLU_en_in             (ReLU_en_in     ),
-        .weight_in              (weight_in      ),
-        .activation_in          (activation_in  ),
-        .bias_in                (bias_in        ),
-        .bias_valid_in          (bias_valid_in  ),
+        .Filter_in_in           (Filter_in      ),
+        .activation_in          (IA_in          ),
+        .CTRL_counter_in        (CTRL_counter_in),
+
         .MAC_out                (MAC_out_wr     ),
         .MAC_valid_out          (MAC_valid_wr   )
     );
@@ -69,18 +59,14 @@ module MAC #(
     parameter ADDR_WIDTH=8
 )
 (
-    input wire clk,
-    input wire rst_n,
-    input wire                         CTRL_gap_enable_in,
-    input wire                         ReLU_en_in,
-    input wire signed [DATA_WIDTH-1:0] weight_in,
-    input wire signed [DATA_WIDTH-1:0] activation_in,
+    input wire                          clk,
+    input wire                          rst_n,
+    input wire signed [DATA_WIDTH-1:0]  Filter_in,
+    input wire signed [DATA_WIDTH-1:0]  IA_in,
+    input wire [2:0]                    CTRL_counter_in,
 
-    input wire signed [DATA_WIDTH-1:0] bias_in,
-    input wire                         bias_valid_in,
-
-    output reg signed [DATA_WIDTH-1:0] MAC_out,
-    output reg                         MAC_valid_out
+    output reg signed [DATA_WIDTH-1:0]  MAC_out,
+    output reg                          MAC_valid_out
 );  
     wire signed [2*DATA_WIDTH-1:0] partial_sum0_wr, partial_sum1_wr, partial_sum2_wr, partial_sum3_wr;
     wire signed[2*DATA_WIDTH-1:0] partial_sum4_wr, partial_sum5_wr, partial_sum6_wr, partial_sum7_wr;
@@ -91,7 +77,6 @@ module MAC #(
     wire signed [DATA_WIDTH-1:0] multiplicand_wr;
     wire signed [DATA_WIDTH-1:0] multiplier_wr;
     wire                         sign_wr;
-    wire signed [DATA_WIDTH-1:0] bias_wr;
     wire signed [DATA_WIDTH-1:0] out_wr;
     wire signed [DATA_WIDTH-1:0] out1_wr;
 
@@ -99,17 +84,12 @@ module MAC #(
     reg signed [DATA_WIDTH-1:0] accumulation_rg;
     reg signed [DATA_WIDTH-1:0] multiplier_rg;
     reg signed [DATA_WIDTH-1:0] multiplicand_rg;
-    reg signed [DATA_WIDTH-1:0] bias_rg;
     reg                         sign_rg;
-    reg                         ReLU_en_rg;
-    reg                         bias_valid_rg;
 //Clock1
     
-    assign multiplicand_wr  =       (CTRL_gap_enable_in)? 16'd64 :
-                                    (weight_in<0) ? (-weight_in) : weight_in;                                              // Absolute value of the weight
-    assign multiplier_wr    =       (activation_in<0) ? (-activation_in) : activation_in;                                    // Absolute value of the activation
-    assign sign_wr          =       (CTRL_gap_enable_in)? activation_in[DATA_WIDTH-1] :
-                                    (weight_in[DATA_WIDTH-1] ^ activation_in[DATA_WIDTH-1]);                          // Determine the sign of the product                                     // Sign-extend the weight to 32 bits
+    assign multiplicand_wr  =       (Filter_in<0) ? (-Filter_in) : Filter_in;                                         
+    assign multiplier_wr    =       (IA_in<0) ? (-IA_in) : IA_in;                                 
+    assign sign_wr          =       (Filter_in[DATA_WIDTH-1] ^ IA_in[DATA_WIDTH-1]);                       
 
     assign partial_sum0_wr = multiplier_wr[0] ? ($signed(multiplicand_wr) << 0) : 0;
     assign partial_sum1_wr = multiplier_wr[1] ? ($signed(multiplicand_wr) << 1) : 0;
@@ -135,40 +115,31 @@ module MAC #(
                             + partial_sum14_wr + partial_sum15_wr + sum_stage1_rg;
 
     assign sum_final_wr = (sign_rg)? $signed(sum_stage2_wr[21:6]+sum_stage2_wr[5:5]): 
-                                    -$signed(sum_stage2_wr[21:6]+sum_stage2_wr[5:5]); // Convert to Q9.6 by shift right or divide by 64 and sign correction
-    assign bias_wr = (bias_valid_rg) ? bias_rg : 0;
-    assign out_wr  = sum_final_wr + bias_wr + accumulation_rg;
+                                    -$signed(sum_stage2_wr[21:6]+sum_stage2_wr[5:5]); 
+    assign out_wr  = sum_final_wr + accumulation_rg;
 
-    assign out1_wr = ((ReLU_en_rg) && (out_wr[DATA_WIDTH-1:DATA_WIDTH-1]==1)) ? 0 : out_wr; // ReLU activation function
+    assign MAC_out =  out_wr;
+    assign MAC_valid_out = (CTRL_counter_in == 4) ? 1 : 0;
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             accumulation_rg <= 0;
             multiplier_rg   <= 0;
             multiplicand_rg <= 0;
-            sum_stage1_rg   <= 0; // Reset cho Reg Pipeline
-            bias_rg         <= 0;
+            sum_stage1_rg   <= 0;                                  
             sign_rg         <= 0;
-            ReLU_en_rg      <= 0;
-            bias_valid_rg   <= 0;
             MAC_out         <= 0;
             MAC_valid_out   <= 0;
         end else begin
-            sum_stage1_rg           <= sum_stage1_wr;
+            sum_stage1_rg           <= sum_stage1_wr;                                                                                      
             multiplicand_rg         <= multiplicand_wr;
             multiplier_rg           <= multiplier_wr;
             sign_rg                 <= sign_wr;
 
-            ReLU_en_rg              <= ReLU_en_in;
-            bias_rg                 <= bias_in;
-            bias_valid_rg           <= bias_valid_in;
-            if (bias_valid_rg) begin 
-                MAC_out         <= out1_wr;
-                MAC_valid_out   <= 1'b1;
-                accumulation_rg <= 0;
+            if(CTRL_counter_in == 0) begin
+                accumulation_rg         <= 0;
             end else begin
-                accumulation_rg <= out_wr;
-                MAC_valid_out   <= 1'b0;
+                accumulation_rg         <= out_wr; 
             end
         end
     end
